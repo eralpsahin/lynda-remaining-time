2c58c81d1e58c6ae7c8f040cf751381bc1933b3f
Eralp Sahin <eralpsahin@me.com>
2019-06-26 23:10:54 -0400

    Add extension options page

--- js/options.js
+++ js/options.js
@@ -1,32 +1,75 @@
-// Saves options to chrome.storage
-function save_options() {
-    let color = document.getElementById('color').value;
-    let likesColor = document.getElementById('like').checked;
-    chrome.storage.sync.set({
-        favoriteColor: color,
-        likesColor: likesColor
-    }, function() {
-        // Update status to let user know options were saved.
-        let status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
-    });
-}
+"use strict";
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
-    chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
-    }, function(items) {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
-    });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+(function() {
+
+    restore_options();
+
+    let total = document.getElementById('total-checkbox');
+    let remaining = document.getElementById('radio1');
+    let completed = document.getElementById('radio2');
+
+    // Saves options to chrome.storage
+    function save_options() {
+        chrome.storage.sync.set({
+            total: total.checked,
+            remaining: remaining.checked
+        }, function() {
+            M.toast({html: 'Settings Updated!', displayLength: 1000})
+            update_preview();
+        });
+    }
+
+    function restore_options() {
+        chrome.storage.sync.get({
+            total: true,
+            remaining: true
+        }, function(items) {
+
+            // Set values according to the fetched storage.
+            if (items.total) {
+                total.checked = true;
+            } else {
+                total.checked = false;
+            }
+            if (items.remaining) {
+                remaining.checked = true;
+            } else {
+                completed.checked = true;
+            }
+            update_preview();
+        });
+    }
+
+    function update_preview() {
+
+        let total_preview = document.getElementById('total-preview');
+        let remaining_preview = document.getElementById('remaining-preview');
+        let completed_preview = document.getElementById('completed-preview');
+        if (!total.checked) {
+            total_preview.style.display = "none";
+            remaining_preview.classList.add("single");
+            completed_preview.classList.add("single");
+
+        } else {
+            total_preview.style.display = "inline-block";
+            remaining_preview.classList.remove("single");
+            completed_preview.classList.remove("single");
+        }
+
+        if (remaining.checked) {
+            remaining_preview.style.display = "inline-block";
+            completed_preview.style.display = "none";
+
+        } else {
+            remaining_preview.style.display = "none";
+            completed_preview.style.display = "inline-block";
+        }
+    }
+
+    document.getElementById('radio1').addEventListener('input',
+        save_options);
+    document.getElementById('radio2').addEventListener('input',
+        save_options);
+    document.getElementById('total-checkbox').addEventListener('input',
+        save_options);
+})();
